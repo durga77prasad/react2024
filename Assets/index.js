ba8af5ea28d10fe1c4b769d6e7a15afe32e0277b
durga77prasad <durga77prasad@gmail.com>
2024-05-26 21:44:39 +0530

    Javascript Example

---
+++ Assets/index.js
@@ -0,0 +1,21 @@
+
+function onLoad() {
+    alert('Welcome');
+}
+
+function headClick() {
+    x=11;
+    alert(x);
+}
+
+function clickMe() {
+    alert("hey you clicked me....");
+}
+
+function add(a,b,c=1) {
+    alert(a+b+c);
+}
+
+function arthemeticOperations() {
+    add(1,2)
+}
